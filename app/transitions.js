15c03a8f02e56638890b9bf3f8123559c9531efd
null null null <null@affinitygamescollective.org>
2016-11-07 09:44:43 -0800

    use flexbox instead of fixed

--- app/transitions.js
+++ app/transitions.js
@@ -1,11 +1,11 @@
 export default function() {
   this.transition(
     this.toRoute(() => true),
-    this.use('scrollThen', 'toRight', { duration: 250 })
+    this.use('toRight')
   );
 
   this.transition(
     this.toRoute((routeName) => (routeName.match(/\./g) || []).length === 0 || routeName === 'index'),
-    this.use('scrollThen', 'toDown')
+    this.use('toDown')
   );
 }
