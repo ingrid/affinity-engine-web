3d2c3a4741a61cd84808e7337e880d76c662d1df
null null null <null@affinitygamescollective.org>
2016-11-12 09:18:51 -0800

    move markdown into translations

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
-      stringifyPath: 'markdown',
+      stringifyPath: 'translations/markdown',
       'ember-font-awesome': {
         useScss: true
       },
