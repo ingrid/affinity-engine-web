1ab5ba881ac33dcd4d876c1a5c4b0732fa1aa87c
null null null <null@affinitygamescollective.org>
2016-12-01 21:05:14 -0800

    transition to layers for ember

--- app/components/engine-container.js
+++ app/components/engine-container.js
@@ -37,23 +37,91 @@ export default Component.extend({
       name: 'Ember',
       namePosition: 'right',
       height: 360,
-      defaultState: { expression: 'neutral' },
+      defaultState: { pose: 'standing', outfit: 'trendy', expression: 'neutral' },
       layerOrder: [
-        'base'
+        'base',
+        'nails',
+        'shoes',
+        'top',
+        'pants',
+        'jacket',
+        'nose',
+        'mouth',
+        'eyes',
+        'brows',
+        'hair'
       ],
       layers: {
         base: [{
-          state: { expression: 'neutral' },
-          keyframe: 'ember-default'
+          state: { pose: 'standing' },
+          keyframe: 'ember-pose-standing'
+        }],
+        nails: [{
+          state: { pose: 'standing', outfit: 'trendy' },
+          keyframe: 'ember-nails-red'
+        }],
+        shoes: [{
+          state: { pose: 'standing', outfit: 'trendy' },
+          keyframe: 'ember-shoes-red-pumps'
+        }],
+        top: [{
+          state: { pose: 'standing', outfit: 'trendy' },
+          keyframe: 'ember-top-red-tank'
+        }],
+        pants: [{
+          state: { pose: 'standing', outfit: 'trendy' },
+          keyframe: 'ember-pants-shredded-jeans'
+        }],
+        jacket: [{
+          state: { pose: 'standing', outfit: 'trendy' },
+          keyframe: 'ember-jacket-red-letter-jacket'
+        }],
+        nose: [{
+          state: { pose: 'standing' },
+          keyframe: 'ember-nose-neutral'
+        }],
+        mouth: [{
+          state: { pose: 'standing', expression: 'neutral' },
+          keyframe: 'ember-mouth-neutral'
         }, {
-          state: { expression: 'smiling' },
-          keyframe: 'ember-smiling'
+          state: { pose: 'standing', expression: 'proud' },
+          keyframe: 'ember-mouth-smile-closed'
         }, {
-          state: { expression: 'proud' },
-          keyframe: 'ember-proud'
+          state: { pose: 'standing', expression: 'smiling' },
+          keyframe: 'ember-mouth-smile-open'
         }, {
-          state: { expression: 'obviously' },
-          keyframe: 'ember-obviously'
+          state: { pose: 'standing', expression: 'obviously' },
+          keyframe: 'ember-mouth-pout'
+        }],
+        eyes: [{
+          state: { pose: 'standing', expression: 'neutral' },
+          keyframe: 'ember-eyes-neutral'
+        }, {
+          state: { pose: 'standing', expression: 'proud' },
+          keyframe: 'ember-eyes-left'
+        }, {
+          state: { pose: 'standing', expression: 'smiling' },
+          keyframe: 'ember-eyes-neutral'
+        }, {
+          state: { pose: 'standing', expression: 'obviously' },
+          keyframe: 'ember-eyes-up-left'
+        }],
+        brows: [{
+          state: { pose: 'standing', expression: 'neutral' },
+          keyframe: 'ember-brows-neutral'
+        }, {
+          state: { pose: 'standing', expression: 'proud' },
+          keyframe: 'ember-brows-up'
+        }, {
+          state: { pose: 'standing', expression: 'smiling' },
+          keyframe: 'ember-brows-up'
+        }, {
+          state: { pose: 'standing', expression: 'obviously' },
+          keyframe: 'ember-brows-asymetrical'
+        }],
+        hair: [{
+          state: { pose: 'standing', outfit: 'trendy' },
+          keyframe: 'ember-hair-red-black-braids'
         }]
       }
     }, {
@@ -85,21 +153,69 @@ export default Component.extend({
       keyframe: 'diy-bedroom'
     }],
     keyframes: [{
-      id: 'ember-default',
-      caption: 'Ember',
-      src: 'affinity-engine/characters/ember/default.png'
+      id: 'ember-pose-standing',
+      src: 'affinity-engine/characters/ember/poses/standing.png'
+      // src: 'affinity-engine/characters/ember/brows/asymetrical.png'
     }, {
-      id: 'ember-smiling',
-      caption: 'Ember',
-      src: 'affinity-engine/characters/ember/smiling.png'
+      id: 'ember-brows-asymetrical',
+      src: 'affinity-engine/characters/ember/brows/asymetrical.png'
     }, {
-      id: 'ember-proud',
-      caption: 'Ember',
-      src: 'affinity-engine/characters/ember/proud.png'
+      id: 'ember-brows-neutral',
+      src: 'affinity-engine/characters/ember/brows/neutral.png'
     }, {
-      id: 'ember-obviously',
-      caption: 'Ember',
-      src: 'affinity-engine/characters/ember/obviously.png'
+      id: 'ember-brows-up',
+      src: 'affinity-engine/characters/ember/brows/up.png'
+    }, {
+      id: 'ember-earing-gold-left',
+      src: 'affinity-engine/characters/ember/earings/gold-left.png'
+    }, {
+      id: 'ember-eyes-left',
+      src: 'affinity-engine/characters/ember/eyes/left.png'
+    }, {
+      id: 'ember-eyes-right',
+      src: 'affinity-engine/characters/ember/eyes/right.png'
+    }, {
+      id: 'ember-eyes-neutral',
+      src: 'affinity-engine/characters/ember/eyes/neutral.png'
+    }, {
+      id: 'ember-eyes-up-left',
+      src: 'affinity-engine/characters/ember/eyes/up-left.png'
+    }, {
+      id: 'ember-hair-red-black-braids',
+      src: 'affinity-engine/characters/ember/hair/red-black-braids.png'
+    }, {
+      id: 'ember-jacket-red-letter-jacket',
+      src: 'affinity-engine/characters/ember/jackets/red-letter-jacket.png'
+    }, {
+      id: 'ember-mouth-neutral',
+      src: 'affinity-engine/characters/ember/mouths/neutral.png'
+    }, {
+      id: 'ember-mouth-pout',
+      src: 'affinity-engine/characters/ember/mouths/pout.png'
+    }, {
+      id: 'ember-mouth-smile-closed',
+      src: 'affinity-engine/characters/ember/mouths/smile-closed.png'
+    }, {
+      id: 'ember-mouth-smile-open',
+      src: 'affinity-engine/characters/ember/mouths/smile-open.png'
+    }, {
+      id: 'ember-nails-natural',
+      src: 'affinity-engine/characters/ember/nails/natural.png'
+    }, {
+      id: 'ember-nails-red',
+      src: 'affinity-engine/characters/ember/mouths/red.png'
+    }, {
+      id: 'ember-nose-neutral',
+      src: 'affinity-engine/characters/ember/noses/neutral.png'
+    }, {
+      id: 'ember-pants-shredded-jeans',
+      src: 'affinity-engine/characters/ember/pants/shredded-jeans.png'
+    }, {
+      id: 'ember-shoes-red-pumps',
+      src: 'affinity-engine/characters/ember/shoes/red-pumps.png'
+    }, {
+      id: 'ember-top-red-tank',
+      src: 'affinity-engine/characters/ember/tops/red-tank.png'
     }, {
       id: 'diy-default',
       caption: 'Diy',
