e745a7e970ca95b917d54f8a9e4a4ef168772373
null null null <null@affinitygamescollective.org>
2017-09-17 16:38:34 -0400

    more upbeat caption demo

--- app/affinity-engine/stage/scenes/direction/text/attribute/caption.js
+++ app/affinity-engine/stage/scenes/direction/text/attribute/caption.js
@@ -4,8 +4,8 @@ import ReplayableScriptMixin from 'affinity-engine-web/mixins/replayable-script'
 
 export default Scene.extend(ReplayableScriptMixin, {
   start: task(function * (script) {
-    yield script.text('engines.direction.text.attribute.caption.withCaption', { caption: 'engines.direction.text.attribute.caption.caption' });
     yield script.text('engines.direction.text.attribute.caption.withoutCaption');
+    yield script.text('engines.direction.text.attribute.caption.withCaption', { caption: 'engines.direction.text.attribute.caption.caption' });
 
     this.get('replay').perform(script);
   })
