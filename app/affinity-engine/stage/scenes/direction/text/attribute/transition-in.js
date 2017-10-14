8ed3fe8ec6f3745dc90ae0493b151a776dba3ed7
ingrid <hi@ingridcheung.com>
2017-10-13 21:24:42 -0400

    Fix transition and tweak dialogue.

--- app/affinity-engine/stage/scenes/direction/text/attribute/transition-in.js
+++ app/affinity-engine/stage/scenes/direction/text/attribute/transition-in.js
@@ -6,9 +6,9 @@ export default Scene.extend(ReplayableScriptMixin, {
     start: task(function * (script) {
       yield script.text('engines.direction.text.attribute.transition-in.announcer1', { caption: 'engines.direction.text.attribute.transition-in.announcer' });
       yield script.text('engines.direction.text.attribute.transition-in.announcer2', { caption: 'engines.direction.text.attribute.transition-in.announcer' });
-      yield script.text('engines.direction.text.attribute.transition-in.announcer3', { caption: 'engines.direction.text.attribute.transition-in.announcer' });
       yield script.text('engines.direction.text.attribute.transition-in.diy1', { caption: 'engines.direction.text.attribute.transition-in.diy' });
-      yield script.text('engines.direction.text.attribute.transition-in.diy2', { caption: 'engines.direction.text.attribute.transition-in.diy', transitionIn: { effect: { translateX: '-100%'}, duration: 500 }});
+      yield script.text('engines.direction.text.attribute.transition-in.announcer3', { caption: 'engines.direction.text.attribute.transition-in.announcer' });
+      yield script.text('engines.direction.text.attribute.transition-in.diy2', { caption: 'engines.direction.text.attribute.transition-in.diy', transitionIn: { effect: { translateX: ['0%', '-100%'], opacity: [1, 1]}, duration: 1200 }, instant: true });
       yield script.text('engines.direction.text.attribute.transition-in.announcer4', { caption: 'engines.direction.text.attribute.transition-in.announcer' });
 
       this.get('replay').perform(script);
